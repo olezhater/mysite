ce22ab492cce175de835aee49bd95d401bdc0fc5
Oleg Teretenko <olezhater@gmail.com>
2023-04-20 17:30:10 +0300

    v0.0.3

--- pages/project-6.js
+++ pages/project-6.js
@@ -28,7 +28,13 @@ const ProjectSix = () => {
         <img src="../img/project-6/6-6.png" className="w-full" />
         <img src="../img/project-6/6-7.png" className="w-full" />
         <img src="../img/project-6/6-8.png" className="w-full" />
-        <video src="../img/project-6/6-9.mov" className="w-full" />
+        <div style={{ position: "relative", paddingBottom: "56.25%", height: 0 }}>
+            <iframe
+                src="https://player.vimeo.com/video/722856848?autoplay=1&loop=1&muted=0&portrait=0&byline=0&h=2697d04fbb&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                allowFullScreen
+                style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
+            />
+        </div>
         <h2 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold" style={{ marginTop: '60px' }}>You May also like</h2>
         <div className="mt-5 grid grid-cols-1 tablet:grid-cols-3 gap-4">
           {reversedProjects.map((p) => (
