4015bee472999e60969fa0e8147c0a362ded7dad
Oleg Teretenko <olezhater@gmail.com>
2023-09-24 17:51:16 +0300

    v.0.0.39

---
+++ components/VideoCompare/index.js
@@ -0,0 +1,70 @@
+import React, { useRef, useState, useEffect } from 'react';
+
+function VideoCompare() {
+  const videoContainerRef = useRef(null);
+  const videoClipperRef = useRef(null);
+  const clippedVideoRef = useRef(null);
+  const sliderRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [sliderValue, setSliderValue] = useState(50);
+
+  useEffect(() => {
+    // Set the initial clip value to 50vw when the component mounts
+    videoClipperRef.current.style.clip = `rect(auto, ${sliderValue}vw, auto, auto)`;
+  }, [sliderValue]);
+
+  function trackLocation(e) {
+    if (!isDragging) return;
+
+    const rect = videoContainerRef.current.getBoundingClientRect();
+    const position = ((e.pageX - rect.left) / videoContainerRef.current.offsetWidth) * 100;
+    if (position >= 0 && position <= 100) {
+      setSliderValue(position);
+    }
+  }
+
+  function startDragging() {
+    setIsDragging(true);
+  }
+
+  function stopDragging() {
+    setIsDragging(false);
+  }
+
+  return (
+    <div id="video-compare-container" ref={videoContainerRef} onMouseMove={trackLocation} onMouseUp={stopDragging} onMouseLeave={stopDragging}>
+      <video loop autoPlay poster="../public/images/before_redesign.jpg">
+        <source src="../public/images/before_redesign.mp4" />
+        <source src="../public/images/before_redesign.webm" />
+      </video>
+      <div
+        id="video-clipper"
+        ref={videoClipperRef}
+        onMouseDown={startDragging}
+        onMouseUp={stopDragging}
+      >
+        <video loop autoPlay poster="../public/images/after_redesign.jpg" ref={clippedVideoRef}>
+          <source src="../public/images/after_redesign.mp4" />
+          <source src="../public/images/after_redesign.webm" />
+        </video>
+      </div>
+      <div
+        className="image-comparison__slider"
+        data-image-comparison-slider=""
+        style={{ left: `${sliderValue}%` }}
+        ref={sliderRef}
+        onMouseDown={startDragging}
+        onMouseUp={stopDragging}
+      >
+        <span className="image-comparison__thumb" data-image-comparison-thumb="">
+          <svg className="image-comparison__thumb-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="10" viewBox="0 0 18 10" fill="currentColor">
+            <path className="image-comparison__thumb-icon--left" d="M12.121 4.703V.488c0-.302.384-.454.609-.24l4.42 4.214a.33.33 0 0 1 0 .481l-4.42 4.214c-.225.215-.609.063-.609-.24V4.703z"></path>
+            <path className="image-comparison__thumb-icon--right" d="M5.879 4.703V.488c0-.302-.384-.454-.609-.24L.85 4.462a.33.33 0 0 0 0 .481l4.42 4.214c.225.215.609.063.609-.24V4.703z"></path>
+          </svg>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default VideoCompare;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "../styles/VideoCompare.css";
 import { ThemeProvider } from "next-themes";
 
 const App = ({ Component, pageProps }) => {
